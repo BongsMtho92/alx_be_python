c6f2fcb21baee4b2404c18359b763c32453893a8
bmthombeni872@gmail.com <bmthombeni872@gmail.com>
2025-05-31 14:23:22 -0700

    alx_be_python project

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,15 @@
+#define the two numbers
+number1 = 10
+number2 = 5
+
+#Do the math
+sum_result = number1 + number2
+difference = number1 - number2
+product_result = number1 * number2
+
+#print the results
+print("addition of", number1, "and", number2, "is", sum_result)
+print("subtraction of", number1, "and", number2, "is", sum_result)
+print("product of", number1, "and", number2, "is", product_result)
+
+
